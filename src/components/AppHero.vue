c317737f5da956a850ed113be78ae70787ab3a30
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-31 16:59:05 +0100

    add title and image

---
+++ src/components/AppHero.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'AppHero'
+}
+</script>
+
+<template>
+    <main>
+        <img src="../assets/img/smile.png" alt="">
+    </main>
+</template>
+
+<style></style>
